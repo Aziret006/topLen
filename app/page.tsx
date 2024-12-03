da18596be48a94a57ab1db48eaad25dc142a6358
aziret <.email@example.com>
2024-12-03 15:58:18 +0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,12 @@ import Header from "@/components/Header/Header";
 import s from "./page.module.scss";
 import Section from "@/components/Section/Section";
 import Main from "@/components/Main/Main";
-// import PageComponent from "@/components/PageComponent/PageComponent";
 import PagePrimus from "@/components/PagePrimus/PagePrimus";
 import TopSlider from "@/components/TopForPC/TopSlider";
 import Mode from "@/components/PageMode/Mode";
-import Footer from "@/components/Footer/Footer";
-import Technical from "@/components/Technical/Technical";
-import Grade from "@/components/Grade/Grade";
 import Trainermode from "@/components/TrainerMode.tsx/Trainermode";
 import Organizer from "@/components/Organizer/Organizer";
-import RoleCarousel from "@/components/PageRoles/Roles";
 import Ecosystem from "@/components/Ecosystem/Ecosystem";
-import Field from "@/components/FooballFiled/Field";
-import Tryout from "@/components/Tryout/Tryout";
-import PageComponent from "@/components/PageComponent/PageComponent";
 
 export default function Home() {
   return (
