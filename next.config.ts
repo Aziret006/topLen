bff61064f564c247a4169055a9424888664c5ce1
Ваше Имя <aziretaalybaev2006@gmail.com>
2024-12-12 14:24:35 +0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'], // Используем @svgr/webpack для импорта SVG как React-компонентов
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
