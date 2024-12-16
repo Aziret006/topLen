ae9cab54f42fe980040c922cc1d85d8249f1e9fd
Ваше Имя <aziretaalybaev2006@gmail.com>
2024-12-16 18:36:48 +0600

    fix

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -74,7 +74,7 @@ const Section = () => {
         </div>
         <div className={styles.phoneImage}>
           <Image
-            src="/2.svg"
+            src="/3.png"
             alt="Превью приложения ТОП на iPhone 14"
             width={539}
             height={830}
@@ -112,7 +112,7 @@ const Section = () => {
           </div>
           <div className={styles.phoneImage}>
             <Image
-              src="/2.svg"
+              src="/3.png"
               alt="Превью приложения ТОП на iPhone 14"
               width={539}
               height={830}
